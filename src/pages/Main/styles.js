edaeedcfc245ae889e63091f77f7de33850e7c77
Marcos de P. Muniz <marcospmuniz@gmail.com>
2019-08-19 22:49:55 -0300

    iniciando o cadastro de usuários

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components/native';
+import {RectButton} from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 30px;
+`;
+
+export const Form = styled.View`
+  flex-direction: row;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-bottom-color: #eee;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  flex: 1;
+  height: 40px;
+  background: #eee;
+  border-radius: 4px;
+  padding: 0 15px;
+  border: 1px solid #eee;
+`;
+
+// o componente RectButton faz parte do pacote react-native-gesture-handler
+// e por isso não tem como estilizar ele da mesma forma que os componentes do
+// styled components. Mas é possível estilizar qualquer componente externo
+// colocando-o dentro de parenteses.
+export const SubmitButton = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #7159c1;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 12px;
+`;
